b29657be8545d715d5727fb8298a6f736315e74f
Riven <4771215@qq.com>
2019-06-13 15:19:10 +0800

    init minilfr ext

---
+++ README.md
@@ -0,0 +1,19 @@
+# MiniLFR
+
+Microbit Makecode extension for Kittenbot MiniLFR
+
+![](icon.png)
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+* for PXT/arcade
+(The metadata above is needed for package search.)
+
+```package
+minilfr=github:Kittenbot/pxt-minilfr
+```
---
+++ main.ts
@@ -0,0 +1,17 @@
+/*
+Riven
+Microbit extension for kittenbot MiniLFR
+load dependency
+"minilfr": "file:../pxt-minilfr"
+*/
+
+//% color="#91a7ff" weight=10 icon="\uf534"
+namespace minilfr {
+
+    //% blockId=minilfr_motor block="Motor Speed|%speed"
+    //% weight=91
+    function motor(speed: number): void {
+
+    }
+
+}
